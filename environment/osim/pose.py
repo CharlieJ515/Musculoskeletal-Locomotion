2e5967aa4e6d7222d43a8e2f08b5121bff44e9a1
Charlie Jang <charlie.jang515@gmail.com>
2025-10-28 17:42:04 +0900

    removed Pose frozen=True as it is intended to add CoordState one at a time. also existing set method allows the mutation of _coord, making frozen misleading

--- environment/osim/pose.py
+++ environment/osim/pose.py
@@ -1,16 +1,18 @@
 from typing import Dict, Optional, Tuple, Iterator
 from dataclasses import dataclass, field
 
+
 @dataclass(frozen=True, slots=True)
 class CoordState:
     q: Optional[float] = None  # position (angle or translation)
     u: Optional[float] = None  # speed (angular or linear velocity)
 
-@dataclass(frozen=True, slots=True)
+
+@dataclass(slots=True)
 class Pose:
     _coord: Dict[str, CoordState] = field(default_factory=dict)
 
-    def set(self, name: str, q: Optional[float]=None, u: Optional[float]=None):
+    def set(self, name: str, q: Optional[float] = None, u: Optional[float] = None):
         self._coord[name] = CoordState(q=q, u=u)
 
     def __iter__(self) -> Iterator[Tuple[str, CoordState]]:
@@ -22,6 +24,6 @@ class Pose:
             raise KeyError(f"Coordinate '{name}' not found in Pose.")
 
         return coord
-    
+
     def __contains__(self, name: str) -> bool:
         return name in self._coord
