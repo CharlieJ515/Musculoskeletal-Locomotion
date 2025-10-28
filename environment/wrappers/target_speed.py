00c42ee7fdd1f089b7502736a55d524d1a26051b
Charlie Jang <charlie.jang515@gmail.com>
2025-10-28 14:20:36 +0900

    implementation of TargetSpeedWrapper. has not been tested yet.

---
+++ environment/wrappers/target_speed.py
@@ -0,0 +1,71 @@
+from typing import TypeVar, Any, SupportsFloat, Tuple
+import dataclasses
+
+import gymnasium as gym
+
+from environment.osim import Observation
+from utils.vec3 import Vec3
+
+ObsType = TypeVar("ObsType")
+ActType = TypeVar("ActType")
+
+
+class TargetSpeedWrapper(gym.Wrapper[Observation, ActType, Observation, ActType]):
+    def __init__(
+        self,
+        env: gym.Env[Observation, ActType],
+        *,
+        speed_range: Tuple[float, float] = (1.2, 1.8),
+        speed_tol: float = 0.10,
+        hold_steps: int = 150,
+    ):
+        super().__init__(env)
+        self.speed_range = speed_range
+        self.speed_tol = speed_tol
+        self.hold_steps = hold_steps
+
+    def reset(
+        self,
+        *,
+        seed: int | None = None,
+        options: dict[str, Any] | None = None,
+    ) -> tuple[Observation, dict[str, Any]]:
+        obs, info = self.env.reset(seed=seed, options=options)
+        # TODO - raise RuntimeError if obs is normalized, warn to use normlization wrapper at last
+
+        self.hold_counter: int = 0
+        self.target_speed: float = self.np_random.uniform(*self.speed_range)
+        self.target_speed = round(self.target_speed, 1)
+
+        # target velocity is relative to the pelvis
+        target_vel = Vec3(self.target_speed, 0, 0)
+        obs = self._set_target_to_obs(obs, target_vel)
+        info["target_speed"] = self.target_speed
+        return obs, info
+
+    @staticmethod
+    def _set_target_to_obs(obs: Observation, target_vel: Vec3) -> Observation:
+        return dataclasses.replace(obs, target_velocity=target_vel)
+
+    def step(
+        self, action: ActType
+    ) -> tuple[Observation, SupportsFloat, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = self.env.step(action)
+
+        pelvis = obs.body["pelvis"]
+        pelvis_vel = pelvis.vel
+        pelvis_speed = (pelvis_vel.x**2 + pelvis_vel.z**2) ** 0.5
+        err = pelvis_speed - self.target_speed
+        reward = -(err**2)
+
+        if abs(err) <= self.speed_tol:
+            self.hold_counter = self.hold_counter + 1
+            terminated = self.hold_counter >= self.hold_steps or terminated
+            info["success"] = terminated
+        else:
+            self.hold_counter = 0
+
+        target_vel = Vec3(self.target_speed, 0, 0)
+        obs = self._set_target_to_obs(obs, target_vel)
+        info["target_speed"] = self.target_speed
+        return obs, reward, terminated, truncated, info
