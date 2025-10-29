f3536d01c29666238867bdc1bdf8c03b1bd583b9
Charlie Jang <charlie.jang515@gmail.com>
2025-10-29 21:55:52 +0900

    added default pose from osim-rl

--- environment/osim/pose.py
+++ environment/osim/pose.py
@@ -27,3 +27,30 @@ class Pose:
 
     def __contains__(self, name: str) -> bool:
         return name in self._coord
+
+
+pose = Pose()
+
+# speeds (QQDot)
+pose.set("pelvis_tx", u=0.0)  # forward speed (QQDot[3])
+pose.set("pelvis_tz", u=0.0)  # rightward speed (QQDot[5])
+
+# pelvis orientations/translations set to zero in reset
+pose.set("pelvis_list", q=0.0)  # QQ[1]
+pose.set("pelvis_rotation", q=0.0)  # QQ[2]
+pose.set("pelvis_ty", q=0.94)  # pelvis height (QQ[4])
+pose.set("pelvis_tilt", q=-0.0)  # trunk lean (+ backward) (QQ[0])
+
+# right leg
+pose.set("hip_adduction_r", q=-0.0)  # abduction is negative of adduction (QQ[7])
+pose.set("hip_flexion_r", q=-0.0)  # flexion is negative in this model (QQ[6])
+pose.set("knee_angle_r", q=0.0)  # extension positive (QQ[13])
+pose.set("ankle_angle_r", q=-0.0)  # “flex” sign per model (QQ[15])
+
+# left leg
+pose.set("hip_adduction_l", q=-0.0)  # (QQ[10])
+pose.set("hip_flexion_l", q=-0.0)  # (QQ[9])
+pose.set("knee_angle_l", q=0.0)  # (QQ[14])
+pose.set("ankle_angle_l", q=-0.0)  # (QQ[16])
+
+osim_rl_pose = pose
