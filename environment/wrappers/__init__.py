9a3de8b203ebc346fa6cddcd59f5cbb2cafc3511
Charlie Jang <charlie.jang515@gmail.com>
2025-10-30 09:15:33 +0900

    added wrappers

--- environment/wrappers/__init__.py
+++ environment/wrappers/__init__.py
@@ -1,1 +1,4 @@
-from .scale_action import ScaleAction
+from .motion_logger import MotionLoggerWrapper
+from .target_speed import TargetSpeedWrapper
+from .target_velocity import TargetVelocityWrapper
+from .simple_env_wrapper import SimpleEnvWrapper
