a2db4ede12b70caaaf66f37808ee96278eefc3e5
Charlie Jang <charlie.jang515@gmail.com>
2025-10-31 03:00:00 +0900

    removed velocity reward as it is moved to reward.py as VelocityReward. now returns velocity information in info["target"]

--- environment/wrappers/target_speed.py
+++ environment/wrappers/target_speed.py
@@ -1,5 +1,6 @@
 from typing import TypeVar, Any, SupportsFloat, Tuple
 import dataclasses
+import warnings
 import math
 
 import gymnasium as gym
@@ -24,6 +25,21 @@ class TargetSpeedWrapper(gym.Wrapper[Observation, ActType, Observation, ActType]
         self.speed_tolerance = speed_tolerance
         self.hold_steps = hold_steps
 
+    @staticmethod
+    def _set_target_to_obs(obs: Observation, target_vel: Vec3) -> Observation:
+        return dataclasses.replace(obs, target_velocity=target_vel)
+
+    def _sample_target(self):
+        self.target_speed: float = round(self.np_random.uniform(*self.speed_range), 1)
+        self.target_vel = Vec3(self.target_speed, 0, 0)
+
+    def _get_distance(self, vel: Vec3, yaw: float) -> float:
+        vel_rot = vel.rotate_y(-yaw)
+        difference = self.target_vel - vel_rot
+        distance = difference.magnitude()
+
+        return distance
+
     def reset(
         self,
         *,
@@ -37,40 +53,50 @@ class TargetSpeedWrapper(gym.Wrapper[Observation, ActType, Observation, ActType]
             )
 
         self.hold_counter: int = 0
-        self.target_speed: float = self.np_random.uniform(*self.speed_range)
-        self.target_speed = round(self.target_speed, 1)
+        self._sample_target()
 
-        # target velocity is relative to the pelvis
-        self.target_vel = Vec3(self.target_speed, 0, 0)
+        pelvis_vel = dataclasses.replace(obs.pelvis.vel, y=0)
+        yaw = obs.pelvis.ang.y
+        dist = self._get_distance(pelvis_vel, yaw)
+
+        in_range = dist <= self.speed_tolerance
+        success = self.hold_counter >= self.hold_steps
+
         obs = self._set_target_to_obs(obs, self.target_vel)
-        info["target_speed"] = self.target_speed
-        return obs, info
+        info["target"] = {
+            "in_range": in_range,
+            "hold_counter": self.hold_counter,
+            "success": success,
+            "target_speed": self.target_speed,
+            "current_speed": pelvis_vel.magnitude(),
+        }
 
-    @staticmethod
-    def _set_target_to_obs(obs: Observation, target_vel: Vec3) -> Observation:
-        return dataclasses.replace(obs, target_velocity=target_vel)
+        return obs, info
 
     def step(
         self, action: ActType
     ) -> tuple[Observation, SupportsFloat, bool, bool, dict[str, Any]]:
         obs, reward, terminated, truncated, info = self.env.step(action)
+        if reward != 0:
+            warnings.warn(
+                "Reward from base environment is being overwritten with reward from TargetSpeedWrapper"
+            )
 
-        pelvis = obs.body["pelvis"]
-        pelvis_vel = pelvis.vel
-        yaw = pelvis.ang.y
-        pelvis_vel_rot = pelvis_vel.rotate_y(-yaw)
-        dv = self.target_vel - pelvis_vel_rot
-        dist = math.hypot(dv.x, dv.z)
+        pelvis_vel = dataclasses.replace(obs.pelvis.vel, y=0)
+        yaw = obs.pelvis.ang.y
+        dist = self._get_distance(pelvis_vel, yaw)
 
-        reward = -(dist**2)
-
-        if dist <= self.speed_tolerance:
-            self.hold_counter = self.hold_counter + 1
-            terminated = self.hold_counter >= self.hold_steps or terminated
-            info["success"] = terminated
-        else:
-            self.hold_counter = 0
+        in_range = dist <= self.speed_tolerance
+        self.hold_counter = self.hold_counter + 1 if in_range else 0
+        success = self.hold_counter >= self.hold_steps
+        terminated = terminated or success
 
         obs = self._set_target_to_obs(obs, self.target_vel)
-        info["target_speed"] = self.target_speed
-        return obs, reward, terminated, truncated, info
+        info["target"] = {
+            "in_range": in_range,
+            "hold_counter": self.hold_counter,
+            "success": success,
+            "target_speed": self.target_speed,
+            "current_speed": pelvis_vel.magnitude(),
+        }
+        return obs, 0.0, terminated, truncated, info
