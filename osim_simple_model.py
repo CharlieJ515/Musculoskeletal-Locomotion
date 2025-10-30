0eb034eeef809fd17b5a7a62f9904a2db3ddfa76
Charlie Jang <charlie.jang515@gmail.com>
2025-10-31 03:02:41 +0900

    script for training simple mlp in opensim as of 2025.10.30

--- osim_simple_model.py
+++ osim_simple_model.py
@@ -1,42 +1,59 @@
 from pathlib import Path
-from typing import Optional, Tuple, cast  
+from typing import Optional, Tuple, cast, List
+import os
 
 import mlflow
 import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
 import numpy as np
-
 import gymnasium as gym
-from gymnasium.spaces import Box
 import opensim
 
-from environment import L2M2019GymEnv
-from environment.wrappers import ScaleAction
-from environment.models import gait14dof22musc
+from environment.models import gait14dof22_path
+from environment.osim import OsimEnv, Action, Observation
+from environment.osim.pose import osim_rl_pose
+from environment.osim.reward import CompositeReward, AliveReward, VelocityReward
+from environment.wrappers import (
+    TargetSpeedWrapper,
+    MotionLoggerWrapper,
+    SimpleEnvWrapper,
+    CompositeRewardWrapper,
+)
 from rl.replay_buffer.replay_buffer import ReplayBuffer
 from rl.sac import SAC, default_target_entropy
 from utils.transition import Transition
 from utils.mlflow_utils import get_tmp, tag_attempt, clear_tmp
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 class MLPActor(nn.Module):
-    def __init__(self, state_dim: Tuple[int, ...], action_dim: Tuple[int, ...], log_std_min: float, log_std_max: float):
+    def __init__(
+        self,
+        state_dim: Tuple[int, ...],
+        action_dim: Tuple[int, ...],
+        log_std_min: float,
+        log_std_max: float,
+    ):
         super().__init__()
         self.state_dim = int(np.prod(state_dim))
         self.action_dim = int(np.prod(action_dim))
         self.log_std_min = log_std_min
         self.log_std_max = log_std_max
 
-        hidden = 256
-        self.net = nn.Sequential(
-            nn.Linear(self.state_dim, hidden), nn.ReLU(),
-            nn.Linear(hidden, hidden), nn.ReLU(),
-        )
-        self.mu_head = nn.Linear(hidden, self.action_dim)
-        self.log_std_head = nn.Linear(hidden, self.action_dim)
+        hidden_dims = [1024, 1024, 512, 256]
+
+        layers = []
+        input_dim = self.state_dim
+        for h in hidden_dims:
+            layers.append(nn.Linear(input_dim, h))
+            layers.append(nn.ReLU())
+            input_dim = h
+
+        self.net = nn.Sequential(*layers)
+        self.mu_head = nn.Linear(hidden_dims[-1], self.action_dim)
+        self.log_std_head = nn.Linear(hidden_dims[-1], self.action_dim)
 
     def forward(self, s: torch.Tensor, deterministic: bool = False):
         x = s.view(s.shape[0], -1)
@@ -68,12 +85,17 @@ class MLPCritic(nn.Module):
         super().__init__()
         self.state_dim = int(np.prod(state_dim))
         self.action_dim = int(np.prod(action_dim))
-        hidden = 256
-        self.q = nn.Sequential(
-            nn.Linear(self.state_dim + self.action_dim, hidden), nn.ReLU(),
-            nn.Linear(hidden, hidden), nn.ReLU(),
-            nn.Linear(hidden, 1),
-        )
+
+        hidden_dims = [1024, 1024, 512, 256, 1]
+
+        layers = []
+        input_dim = self.state_dim + self.action_dim
+        for h in hidden_dims:
+            layers.append(nn.Linear(input_dim, h))
+            layers.append(nn.ReLU())
+            input_dim = h
+
+        self.q = nn.Sequential(*layers)
 
     def forward(self, s: torch.Tensor, a: torch.Tensor):
         x = torch.cat([s.view(s.shape[0], -1), a.view(a.shape[0], -1)], dim=-1)
@@ -82,13 +104,14 @@ class MLPCritic(nn.Module):
 
 @torch.no_grad()
 def evaluate(
-    env: gym.Env,
+    simple_env: gym.Env,
     agent: SAC,
     step: int,
     episodes: int = 5,
 ):
+
     active_run = mlflow.active_run()
-    active_run_name = active_run.data.tags.get("mlflow.runName") # type: ignore[reportOptionalMemberAccess]
+    active_run_name = active_run.data.tags.get("mlflow.runName")  # type: ignore[reportOptionalMemberAccess]
 
     agent.eval()
     gamma = agent.gamma
@@ -96,6 +119,11 @@ def evaluate(
     tmpdir = get_tmp()
     returns = []
 
+    motion_output_dir = tmpdir / f"{active_run_name}_eval_step{step}"
+    env = MotionLoggerWrapper(
+        simple_env,
+        motion_output_dir,
+    )
     for ep in range(episodes):
         run_name = f"{active_run_name}_eval_step{step}_{ep}"
         eval_dir = tmpdir / run_name
@@ -109,23 +137,23 @@ def evaluate(
         ep_disc = 0.0
         disc_pow = 1.0
 
-        s, info = env.reset()
-        done = False
+        s_np, info = env.reset()
         ep_step = 0
-        while not done:
-            st = torch.tensor(s, dtype=torch.float32, device=agent.device).unsqueeze(0)
+        while True:
+            s_t = torch.tensor(
+                s_np, dtype=torch.float32, device=agent.device
+            ).unsqueeze(0)
 
-            a = agent.select_action(st)
-            a_np = a.squeeze(0).cpu().numpy()
-            s_next, r, terminated, truncated, info = env.step(a_np)
-            done = terminated or truncated
+            a_t = agent.select_action(s_t)
+            a_np = a_t.squeeze(0).cpu().numpy()
+            s_next_np, r, terminated, truncated, info = env.step(a_np)
             ep_ret += float(r)
 
             ep_disc += disc_pow * float(r)
             disc_pow *= float(gamma)
 
-            q1 = agent.Q1(st, a).detach().cpu().item()
-            q2 = agent.Q2(st, a).detach().cpu().item()
+            q1 = agent.Q1(s_t, a_t).detach().cpu().item()
+            q2 = agent.Q2(s_t, a_t).detach().cpu().item()
             qmin = min(q1, q2)
 
             mlflow.log_metrics(
@@ -137,13 +165,18 @@ def evaluate(
                     "Q2": float(q2),
                     "Qmin": float(qmin),
                 },
-                step=ep_step
+                step=ep_step,
             )
 
             all_actions.append(a_np)
 
-            s = s_next
+            s_np = s_next_np
             ep_step += 1
+
+            if terminated or truncated:
+                mot_path = info["mot_path"]
+                mlflow.log_artifact(str(mot_path), artifact_path="motion")
+                break
 
         # log total returns
         mlflow.log_metrics(
@@ -152,7 +185,7 @@ def evaluate(
                 "total_discounted_return": float(ep_disc),
                 "episode_length": int(ep_step),
             },
-            step=ep_step
+            step=ep_step,
         )
 
         # plot and log action distribution
@@ -172,61 +205,70 @@ def evaluate(
     agent.train(True)
     return float(np.mean(returns))
 
-def save_ckpt(agent:SAC, ckpt_name:str):
+
+def save_ckpt(agent: SAC, ckpt_name: str):
     tmpdir = get_tmp()
-    ckpt_path = tmpdir / f"{ckpt_name}.pt"
+    if not ckpt_name.endswith(".pt"):
+        ckpt_name = f"{ckpt_name}.pt"
+    ckpt_path = tmpdir / ckpt_name
     agent.save(ckpt_path)
     mlflow.log_artifact(str(ckpt_path), artifact_path="checkpoints")
     print(f"Saved checkpoint")
 
+
 def main():
-    seed = 0
+    seed = 42
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     experiment_name = "SAC-Osim"
     run_name = "simple sac"
-    model = gait14dof22musc
-    difficulty = 0
+    model = gait14dof22_path
+    pose = osim_rl_pose
 
     mlflow.set_tracking_uri("https://mlflow.kyusang-jang.com/capstone")
     mlflow.set_experiment(experiment_name)
     mlflow.start_run(run_name=run_name)
     tag_attempt(experiment_name, run_name)
 
-    mlflow.log_params({
-        "env_id": "L2M2019Env",
-        "opensim_model": str(model),
-        "difficulty": difficulty,
-        "seed": seed,
-        "gym_version": gym.__version__,
-        "opensim_version": opensim.__version__,
-        "torch_version": torch.__version__,
-        "numpy_version": np.__version__,
-    })
+    mlflow.log_params(
+        {
+            "env_id": "OsimEnv",
+            "opensim_model": str(model),
+            "seed": seed,
+            "gym_version": gym.__version__,
+            "opensim_version": opensim.__version__,
+            "torch_version": torch.__version__,
+            "numpy_version": np.__version__,
+        }
+    )
 
     torch.manual_seed(seed)
     np.random.seed(seed)
-    env = L2M2019GymEnv(model_path=model, difficulty=difficulty, visualize=False, seed=seed)
+    osim_env = OsimEnv(model, pose, visualize=True)
+    target_env = TargetSpeedWrapper(osim_env)
+    reward_components = {
+        "alive_reward": AliveReward(1.0),
+        "velocity_reward": VelocityReward(1.0),
+    }
+    reward_weights = {
+        "alive_reward": 10.0,
+        "velocity_reward": 1.0,
+    }
+    reward_fn = CompositeReward(reward_components, reward_weights)
+    reward_env = CompositeRewardWrapper(target_env, reward_fn)
+    simple_env = SimpleEnvWrapper(reward_env)
+    env = gym.wrappers.RescaleAction(simple_env, np.float32(-1.0), np.float32(1.0))
 
-    # wrap env with ScaleAction action wrapper
-    act_space = cast(Box, env.action_space)
-    act_type = act_space.dtype.type
-    src_low = -np.ones_like(act_space.low,  dtype=act_type)
-    src_high = np.ones_like(act_space.high, dtype=act_type)
-    source_space = Box(low=src_low, high=src_high, dtype=act_type)
-    env = ScaleAction(env, source_space)
-    env.action_space.seed(seed)
-
-    # log observation / action space size and range
-    # log after wrapping because wrapper modifies the action_space
     obs_shape = cast(Tuple[int, ...], env.observation_space.shape)
     act_shape = cast(Tuple[int, ...], env.action_space.shape)
-    rew_shape = (1, )
+    rew_shape = (1,)
 
-    mlflow.log_params({
-        "obs_shape": obs_shape,
-        "act_shape": act_shape,
-        "rew_shape": rew_shape,
-    })
+    mlflow.log_params(
+        {
+            "obs_shape": obs_shape,
+            "act_shape": act_shape,
+            "rew_shape": rew_shape,
+        }
+    )
 
     # SAC hyperparams
     agent = SAC(
@@ -265,39 +307,46 @@ def main():
     start_random = 1_000
     batch_size = 256
     eval_interval = 5_000
-    mlflow.log_params({
-        "total_steps": total_steps,
-        "start_random": start_random,
-        "batch_size": batch_size,
-        "eval_interval": eval_interval,
-    })
+    mlflow.log_params(
+        {
+            "total_steps": total_steps,
+            "start_random": start_random,
+            "batch_size": batch_size,
+            "eval_interval": eval_interval,
+        }
+    )
 
     agent.train()
-    s, info = env.reset(seed=seed)
+    s_np, info = env.reset(seed=seed)
     ep_return, ep_len = 0.0, 0
     for t in range(1, total_steps + 1):
         if t < start_random:
-            a = env.action_space.sample()
+            a_np = env.action_space.sample()
         else:
-            st = torch.tensor(s, dtype=torch.float32, device=agent.device).unsqueeze(0)
-            a_t = agent.select_action(st)
-            a = a_t.squeeze(0).cpu().numpy()
+            s_t = torch.tensor(
+                s_np, dtype=torch.float32, device=agent.device
+            ).unsqueeze(0)
+            a_t = agent.select_action(s_t)
+            a_np = a_t.squeeze(0).cpu().numpy()
 
-        s_next, r, terminated, truncated, info = env.step(a)
-        done = terminated or truncated
-        ep_return += float(r); ep_len += 1
+        s_next_np, r, terminated, truncated, info = env.step(a_np)
+
+        ep_return += float(r)
+        ep_len += 1
+        mlflow.log_metrics(info["rewards"], step=t)
+        mlflow.log_metrics(info["target"], step=t)
 
         # Store transition
         tr = Transition(
-            obs=torch.as_tensor(s, dtype=torch.float32),
-            action=torch.as_tensor(a, dtype=torch.float32),
+            obs=torch.as_tensor(s_np, dtype=torch.float32),
+            action=torch.as_tensor(a_np, dtype=torch.float32),
             reward=torch.as_tensor([r], dtype=torch.float32),
-            next_obs=torch.as_tensor(s_next, dtype=torch.float32),
-            done=torch.as_tensor(done, dtype=torch.bool),
+            next_obs=torch.as_tensor(s_next_np, dtype=torch.float32),
+            done=torch.as_tensor(terminated, dtype=torch.bool),
         )
         rb.add(tr)
 
-        s = s_next
+        s_np = s_next_np
 
         # Update
         if t >= start_random:
@@ -306,9 +355,11 @@ def main():
             mlflow.log_metrics(metrics, step=t)
 
         # Episode reset
-        if done:
-            print(f"Episode finished at step {t:6d} | length={ep_len} | return={ep_return:.2f}")
-            s, info = env.reset()
+        if terminated or truncated:
+            print(
+                f"Episode finished at step {t:6d} | length={ep_len} | return={ep_return:.2f}"
+            )
+            s_np, info = env.reset()
             ep_return, ep_len = 0.0, 0
 
         # Periodic eval
@@ -327,9 +378,10 @@ def main():
     # Save checkpoint
     save_ckpt(agent, "sac_osim.pt")
 
+
 if __name__ == "__main__":
     try:
         main()
     finally:
-        mlflow.end_run() # mlflow.end_run is idempotent
+        mlflow.end_run()  # mlflow.end_run is idempotent
         clear_tmp()
