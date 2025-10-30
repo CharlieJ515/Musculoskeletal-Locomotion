47999fdbc8214278127413edfd732890a234dbb1
Charlie Jang <charlie.jang515@gmail.com>
2025-10-31 03:02:01 +0900

    added CompositeRewardWrapper

--- environment/wrappers/__init__.py
+++ environment/wrappers/__init__.py
@@ -2,3 +2,4 @@ from .motion_logger import MotionLoggerWrapper
 from .target_speed import TargetSpeedWrapper
 from .target_velocity import TargetVelocityWrapper
 from .simple_env import SimpleEnvWrapper
+from .composite_reward import CompositeRewardWrapper
