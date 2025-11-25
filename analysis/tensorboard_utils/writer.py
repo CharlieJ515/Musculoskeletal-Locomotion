d224d9208f11a160b7be35601db80aaf48e97be7
Charlie Jang <charlie.jang515@gmail.com>
2025-11-25 12:56:28 +0900

    lazy writer initialization to prevent duplicate writers in multiprocessing

--- analysis/tensorboard_utils/writer.py
+++ analysis/tensorboard_utils/writer.py
@@ -1,7 +1,11 @@
 from torch.utils.tensorboard import SummaryWriter
 
-_writer = SummaryWriter()
+_writer = None
 
 
 def get_writer() -> SummaryWriter:
+    global _writer
+
+    if _writer is None:
+        _writer = SummaryWriter()
     return _writer
